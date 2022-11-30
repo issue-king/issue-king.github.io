f6e69bc9a873bbc665ef51770632b6c093e832ca
zhaoybc <zhaoybc@yonyou.com>
2022-11-30 10:34:57 +0800

    deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -8,8 +8,7 @@ md docs
 xcopy /s /e  source_code\docs\.vuepress\dist docs
 echo(===================== dir copy success =====================)
 
-set msg = %0
 git add .
-git commit -m msg
+git commit -m %0
 git push
 echo(===================== git push success =====================)
